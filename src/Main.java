fcffd3ca981256545035bbd22f693b0a6d0318cb
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-08-01 05:11:37 +0000

    Solid and hollow rectangle added

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main{
+    public static void main(String[] args) {
+        
+        //SOLID RECTANGLE
+        int n = 4;
+        int m = 5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        // }
+
+        //HOLLOW RECTANGLE
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(i == 1 || j == 1 || i == n || j == m){
+                    System.out.print("* ");
+                } else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
